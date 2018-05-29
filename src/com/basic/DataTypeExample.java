84d7e7ca8e1217fa6fc38c38692811a6ebb294ca
balajibadboy <balajicse005@gmail.com>
2018-05-29 03:08:01 -0700

    oii

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+public static void main(String args[])
+{
+	String first="bad";
+	String second="boy";
+	System.out.println(first+second);
+}
+}
